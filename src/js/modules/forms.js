808da82c07676e80f5af3251075a86f81df8aacc
Arthur <arthurrebar8@gmail.com>
2023-03-29 22:47:09 +0300

    forms ajax done

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,63 +1,107 @@
 // import checkNumInputs from "./checkNumInputs";
 
-const forms = (state) => {
+const forms = () => {
 	const form = document.querySelectorAll('form'),
-		  inputs = document.querySelectorAll('input');
+		  inputs = document.querySelectorAll('input'),
+		  upload = document.querySelectorAll('[name="upload"]');
 	
 	
-	checkNumInputs('input[name = "user_phone"]');
+	//checkNumInputs('input[name = "user_phone"]');
 
 	const message = {
 		loading: "Загрузка...",
 		success: "Спасибо, мы скоро с вами свяжемся!",
-		failure: "Ошибка",
-		notEnoughData: "Выберите или заполните все поля"
+		failure: "Что-то пошло не так...",
+		spinner: 'assets/img/spinner.gif',
+		ok: 'assets/img/ok.png',
+		fail: 'assets/img/fail.png'
+	};
+
+	const path = {
+		designer: 'assets/server.php',
+		question :'assets/question.php'
 	};
 
 	const postData = async (url, data) => {
-		document.querySelector('.status').textContent = message.loading;
 		let res = await fetch(url, {
 			method: "POST",
 			body: data
 		});
 
 		return await res.text();
-	}
+	};
 
 	const clearInputs = () => {
 		inputs.forEach(item => {
 			item.value = '';
 		});
-	}
+		upload.forEach(item => {
+			item.previousElementSibling.textContent = 'Файл не выбран';
+		});
+	};
+
+	upload.forEach(item => {
+		item.addEventListener('input', () => {
+			let dots;
+			const arr = item.files[0].name.split('.');
+			arr[0].length > 7 ? dots = '...' : dots = '.'; 
+			const name = arr[0].substr(0, 7) + dots + arr[1];
+			item.previousElementSibling.textContent = name;
+		});
+	});
 	
 
 	form.forEach(item => {
 		item.addEventListener('submit', (e) => {
 			e.preventDefault();
 
+			item.parentNode.style.height = getComputedStyle(item.parentNode).height;
+
 			let statusMessage = document.createElement('div');
 			statusMessage.classList.add('status');
-			item.appendChild(statusMessage);
+			item.parentNode.appendChild(statusMessage);
+
+			item.classList.add('animated', 'fadeOut');
+			setTimeout(() => {
+				item.style.display = 'none';
+			}), 2000;
+
+			let statusImg = document.createElement('img');
+			statusImg.setAttribute('src', message.spinner);
+			statusImg.classList.add('animated', 'fadeInUp');
+			statusMessage.appendChild(statusImg);
+
+			let textMessage = document.createElement('div');
+			textMessage.textContent = message.loading;
+			textMessage.classList.add('animated', 'fadeInUp');
+			statusMessage.appendChild(textMessage);
 
 			const formData = new FormData(item);
-			if (item.getAttribute('data-calc') === "end") {
-				for (let key in state) {
-					formData.append(key, state[key]);
-				}
-			}
 
-			postData('assets/server.php', formData)
+			let api;
+			item.closest('.popup-design') || item.classList.contains('calc_form') ? api = path.designer : api = path.question;
+			console.log(api);
+
+			postData(api, formData)
 				.then(res => {
 					console.log(res);
-					statusMessage.textContent = message.success;
+					statusImg.setAttribute('src', message.ok);
+					textMessage.textContent = message.success;
 				})
-				.catch(() => statusMessage.textContent = message.failure)
+				.catch(() => {
+					statusImg.setAttribute('src', message.fail);
+					textMessage.textContent = message.failure;
+				})
 				.finally(() => {
 					clearInputs();
 					setTimeout(() => {
 						statusMessage.remove();
-					},5000)
-				})
+						item.parentNode.style.removeProperty('height');
+						item.style.display = 'block';
+						item.classList.remove('fadeOut');
+						item.classList.add('fadeInUp');
+					},5000);
+				});
 
 
 		});
